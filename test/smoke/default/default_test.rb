d8a3ba90998fbd5c8d2d0c7355a0207a275fedab
Adam Linkous <alinkous@gmail.com>
2017-03-25 20:11:30 -0500

    Get rpm (#2)
    * test code added and failed as expected
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>
    
    * fix line endings
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>
    
    * put back original long line for sh command
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>
    
    * add code to download plex rpm
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -9,3 +9,13 @@
 describe package('wget') do
   it { should be_installed }
 end
+
+# download directory exists
+describe directory('/tmp') do
+  it { should be_directory }
+end
+
+# plex rpm file exists
+describe file('/tmp/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm') do
+  it { should be_file }
+end
