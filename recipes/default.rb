ea30dd271b68829a6a6755ea27a579dde47500f1
Adam Linkous <alinkous@gmail.com>
2017-03-25 23:49:01 -0500

    Install plex (#3)
    * update tests and use file_cache_path config item
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>
    
    * add code to install plex, update test code
    
    also add .rubocop.yml file to control cookstyle warnings about carriage
    return line endings since that should be managed by git
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>
    
    * update readme
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,19 @@ end
 
 # download plex rpm
 remote_file 'plex-rpm_download' do
-  not_if { ::File.exist?('/tmp/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm') }
-  path '/tmp/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm'
+  not_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm") }
+  path "#{Chef::Config[:file_cache_path]}/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm"
   source 'https://downloads.plex.tv/plex-media-server/1.4.4.3495-edef59192/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm'
+end
+
+# install plex rpm
+package 'plex-rpm' do
+  source "#{Chef::Config[:file_cache_path]}/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm"
+  action :install
+  notifies :restart, 'service[plexmediaserver]'
+end
+
+# plexmediaserver service
+service 'plexmediaserver' do
+  action :enable
 end
