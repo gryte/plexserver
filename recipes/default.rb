50e2241f22419767821262a21ae9e9ac0cdef8ec
Adam Linkous <alinkous@gmail.com>
2017-03-25 20:07:25 -0500

    add code to download plex rpm
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,10 @@
 package 'wget' do
   action :install
 end
+
+# download plex rpm
+remote_file 'plex-rpm_download' do
+  not_if { ::File.exist?('/tmp/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm') }
+  path '/tmp/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm'
+  source 'https://downloads.plex.tv/plex-media-server/1.4.4.3495-edef59192/plexmediaserver-1.4.4.3495-edef59192.x86_64.rpm'
+end
