eef2287e08fb969a51f045d7d68242126556f17e
Adam Linkous <alinkous@gmail.com>
2017-04-30 10:28:26 -0500

    add firewall cookbook; add nfs ports
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,18 +4,10 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'all_rights'
 description 'Installs/Configures plexserver'
 long_description 'Installs/Configures plexserver'
-version '0.7.1'
+version '0.8.0'
 supports 'centos'
 chef_version '~> 12.19' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
 issues_url 'https://github.com/gryte/plexserver/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
 source_url 'https://github.com/gryte/plexserver' if respond_to?(:source_url)
+
+depends 'firewall', '~> 2.6.1'
