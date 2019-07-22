56908779a2212e9b2e8524ddd55bf04978ed5dbd
Adam Linkous <alinkous@gmail.com>
2019-07-21 19:08:41 -0500

    Update Plex Server Version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 # Plex Media Server Package
-default['plexserver']['package']['name'] = 'plexmediaserver-1.15.8.1198-eadbcbb45.x86_64.rpm'
-default['plexserver']['version']['url'] = 'https://downloads.plex.tv/plex-media-server-new/1.15.8.1198-eadbcbb45/redhat/plexmediaserver-1.15.8.1198-eadbcbb45.x86_64.rpm'
+default['plexserver']['package']['name'] = 'plexmediaserver-1.16.2.1321-ad17d5f9e.x86_64.rpm'
+default['plexserver']['version']['url'] = 'https://downloads.plex.tv/plex-media-server-new/1.16.2.1321-ad17d5f9e/redhat/plexmediaserver-1.16.2.1321-ad17d5f9e.x86_64.rpm'
