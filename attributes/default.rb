6449004c447206825463fe7e1a079871b8b5e51f
Adam Linkous <alinkous@gmail.com>
2019-09-23 19:11:50 -0500

    Bump Plex Server Version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 # Plex Media Server Package
-default['plexserver']['package']['name'] = 'plexmediaserver-1.16.5.1554-1e5ff713d.x86_64.rpm'
-default['plexserver']['version']['url'] = 'https://downloads.plex.tv/plex-media-server-new/1.16.5.1554-1e5ff713d/redhat/plexmediaserver-1.16.5.1554-1e5ff713d.x86_64.rpm'
+default['plexserver']['package']['name'] = 'plexmediaserver-1.16.6.1592-b9d49bdb7.x86_64.rpm'
+default['plexserver']['version']['url'] = 'https://downloads.plex.tv/plex-media-server-new/1.16.6.1592-b9d49bdb7/redhat/plexmediaserver-1.16.6.1592-b9d49bdb7.x86_64.rpm'
