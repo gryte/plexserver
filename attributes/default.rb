c3c91f1f53063a6ae68818ab905868d144f5af25
Adam Linkous <alinkous@gmail.com>
2019-10-22 18:50:37 -0500

    Bump Cookbook Version And Update PlexServer Version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 # Plex Media Server Package
-default['plexserver']['package']['name'] = 'plexmediaserver-1.17.0.1709-982421575.x86_64.rpm'
-default['plexserver']['version']['url'] = 'https://downloads.plex.tv/plex-media-server-new/1.17.0.1709-982421575/redhat/plexmediaserver-1.17.0.1709-982421575.x86_64.rpm'
+default['plexserver']['package']['name'] = 'plexmediaserver-1.18.0.1944-f2cae8d6b.x86_64.rpm'
+default['plexserver']['version']['url'] = 'https://downloads.plex.tv/plex-media-server-new/1.18.0.1944-f2cae8d6b/redhat/plexmediaserver-1.18.0.1944-f2cae8d6b.x86_64.rpm'
