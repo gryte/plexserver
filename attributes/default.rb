a84565bbd557a6f45df5adb4f79198231433d79a
Adam Linkous <alinkous@gmail.com>
2019-11-03 09:16:08 -0600

    Update Plex Media Server Version With Cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 # Plex Media Server Package
-default['plexserver']['package']['name'] = 'plexmediaserver-1.18.0.1944-f2cae8d6b.x86_64.rpm'
-default['plexserver']['version']['url'] = 'https://downloads.plex.tv/plex-media-server-new/1.18.0.1944-f2cae8d6b/redhat/plexmediaserver-1.18.0.1944-f2cae8d6b.x86_64.rpm'
+default['plexserver']['package']['name'] = 'plexmediaserver-1.18.1.1973-0f4abfbcc.x86_64.rpm'
+default['plexserver']['version']['url'] = 'https://downloads.plex.tv/plex-media-server-new/1.18.1.1973-0f4abfbcc/redhat/plexmediaserver-1.18.1.1973-0f4abfbcc.x86_64.rpm'
